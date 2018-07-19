0824f52f9f5619fd07194d2d9f27f6524cba2c05
Sara Muntean <saramuntean@Saras-MacBook-Pro.local>
2018-07-19 10:51:09 -0400

    Created a tip calculator that only calculates tip amount.

--- js101.js
+++ js101.js
@@ -20,6 +20,24 @@ function madlib(name, subject) {
 
 console.log(madlib("Sara", "math"));
 
+function tipAmount(bill, serviceLevel) {
+    var tipAmount = 0
+    var tipPercent = 0
+    if (serviceLevel == "good" || "Good") {
+        tipPercent += 0.20;
+    } else if (serviceLevel == "fair" || "Fair") {
+        tipPercent += 0.15;
+    } else if (serviceLevel == "bad" || "Bad") {
+        tipPercent += 0.10;
+    } else {
+        return "Please enter either 'good', 'bad', or 'fair'.";
+    }
+    tipAmount += bill * tipPercent;
+    return tipAmount;
+}
+
+console.log(tipAmount(100, 'good'));
+
 function totalAmount(bill, serviceLevel) {
     var total = 0
     var tipPercent = 0
@@ -36,4 +54,5 @@ function totalAmount(bill, serviceLevel) {
     return total;
 }
 
-console.log(tipAmount(100, 'good'));
+console.log(totalAmount(100, 'good'));
+

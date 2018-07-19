3154a92ea0ea808793cc4ede3abaf9e5786ca8ed
Sara Muntean <saramuntean@Saras-MacBook-Pro.local>
2018-07-19 14:23:20 -0400

    Added a function that draws the perimeter of a box given dimensions.

--- js102.js
+++ js102.js
@@ -14,9 +14,34 @@ var printSquare = function (num) {
         for (var x = 1; x <= num; x++) {
           row += "*";
         };
-      console.log(row);
+    console.log(row);
     };
   };
   
   printSquare(5)
+
+
+  var printBox = function (width, height) {
+    var totalBox = ""
+      for (var y = 1; y <= height; y++) {
+          if (y === 1 || y === height) {
+              var topRow = ""
+              for (var x = 1; x <= width; x++) {
+                  topRow += "*"; 
+              };
+              totalBox += topRow + "\n";
+          } else {
+          var row = "*"
+          for (var x = 1; x <= width - 2; x++) {
+                  row += " ";
+          };
+          row += "*";
+          totalBox += row + "\n";
+          };
+      };
+      console.log(totalBox);
+  };
+  
+  printBox(5, 4)
+
 

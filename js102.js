5314e0c7c16ce542dd00f1b1694e1a48085b3f58
Sara Muntean <saramuntean@Saras-MacBook-Pro.local>
2018-07-19 14:44:35 -0400

    Reworked the box problem to limit the number of lines of code to solve the problem.

--- js102.js
+++ js102.js
@@ -20,28 +20,45 @@ var printSquare = function (num) {
   
   printSquare(5)
 
-
-  var printBox = function (width, height) {
-    var totalBox = ""
-      for (var y = 1; y <= height; y++) {
-          if (y === 1 || y === height) {
-              var topRow = ""
-              for (var x = 1; x <= width; x++) {
-                  topRow += "*"; 
-              };
-              totalBox += topRow + "\n";
-          } else {
-          var row = "*"
-          for (var x = 1; x <= width - 2; x++) {
-                  row += " ";
-          };
-          row += "*";
-          totalBox += row + "\n";
-          };
-      };
-      console.log(totalBox);
-  };
+//  First attempt: 
+//   var printBox = function (width, height) {
+//     var totalBox = ""
+//       for (var y = 1; y <= height; y++) {
+//           if (y === 1 || y === height) {
+//               var topRow = ""
+//               for (var x = 1; x <= width; x++) {
+//                   topRow += "*"; 
+//               };
+//               totalBox += topRow + "\n";
+//           } else {
+//           var row = "*"
+//           for (var x = 1; x <= width - 2; x++) {
+//                   row += " ";
+//           };
+//           row += "*";
+//           totalBox += row + "\n";
+//           };
+//       };
+//       console.log(totalBox);
+//   };
   
-  printBox(5, 4)
+//   printBox(5, 4)
 
 
+// Secon attempt: 
+var printBox = function (width, height) {
+  for (var x = 0; x < width; x++) {
+    var star = "";
+    for (var j = 0; j < height; 1) {
+            if (x === 0 || x === width - 1) {
+                star += "*";
+            } else if (j === 0 || j === height - 1) { 
+                star += "*";
+            } else {
+                star += " ";
+            };
+    console.log(star);
+    };
+  };
+};
+printBox(5, 4)

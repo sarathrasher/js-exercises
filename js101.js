b719041bab33a0e500cc3f5b67e546c8397321d3
Sara Muntean <saramuntean@Saras-MacBook-Pro.local>
2018-07-19 10:33:30 -0400

    Added a madlib function.

--- js101.js
+++ js101.js
@@ -1,16 +1,21 @@
 function hello(name) {
     console.log("Hello, " + name + "!");
 }
-hello("Mustache")
+hello("Mustache");
 
 function hello(name) {
     if (name == null) {
-        console.log("Hello, world!")
+        console.log("Hello, world!");
     } else {
         console.log("Hello, " + name + "!");
     }
 }
 
-hello()
+hello();
 
+function madlib(name, subject) {
+    var string = name + "'s favorite subject is " + subject + ".";
+    return string;
+}
 
+console.log(madlib("Sara", "math"));

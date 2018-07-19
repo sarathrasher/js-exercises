eb5e7500f3ea85c2d9a1289bd190e18aa8f2360f
Sara Muntean <saramuntean@Saras-MacBook-Pro.local>
2018-07-19 10:22:30 -0400

    Created two versions of a 'Hello, world' function.

---
+++ js101.js
@@ -0,0 +1,16 @@
+function hello(name) {
+    console.log("Hello, " + name + "!");
+}
+hello("Mustache")
+
+function hello(name) {
+    if (name == null) {
+        console.log("Hello, world!")
+    } else {
+        console.log("Hello, " + name + "!");
+    }
+}
+
+hello()
+
+

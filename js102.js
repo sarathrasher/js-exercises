0b14ddc036dc99a2469f46c981992c0382f7d67f
Sara Muntean <saramuntean@Saras-MacBook-Pro.local>
2018-07-20 11:14:17 -0400

    Added a function to multiply matrices.

--- js102.js
+++ js102.js
@@ -138,10 +138,10 @@ var decipher = function decipher(cipherString, offset) {
             if (cipherString[i] === lowercase[j] || cipherString[i] === uppercase[j]) {
                 isLetter = true;
                 var letterIndex = j;
-                if (letterIndex >= 13) {
-                    decodeIndex = (26 - (letterIndex - offset)) % 26;
+                if (letterIndex >= offset) {
+                    decodeIndex = (letterIndex - offset) % 26;
                 } else {
-                    decodeIndex = (26 -(letterIndex - offset)) % 26;
+                    decodeIndex = (26 - (letterIndex - offset)) % 26;
                 }
             }
             if (cipherString[i] === uppercase[j]) {
@@ -271,3 +271,30 @@ var addMatrix = function addMatrix(mat1, mat2) {
    
 
 console.log(addMatrix(matrix1, matrix2));
+
+
+//  Matrix Multiplication
+var matrix1 = [ [2, 4,],
+                [3,4] ]
+var matrix2 = [[5,2],
+                [3,1] ]
+
+var multMatrix = function multMatrix(mat1, mat2) {
+    var matProduct = [];
+    var height = mat1.length;
+    var width = mat2[0].length;
+
+    for (var i = 0; i < height; i++) {
+        matProduct.push([]);
+        for (var j = 0; j < width; j++) {
+            matProduct[i].push(0);
+            for (var k = 0; k < mat1[0].length; k++) {
+                var product = matrix1[i][k] * matrix2[k][j];
+                matProduct[i][j] += product;
+            }   
+        }  
+    }
+    return matProduct
+};
+
+console.log(multMatrix(matrix1, matrix2));

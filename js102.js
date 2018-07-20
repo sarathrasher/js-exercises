a8a1203991fdc939b0d6f136eeb5dff53cd859fd
Sara Muntean <saramuntean@Saras-MacBook-Pro.local>
2018-07-20 10:27:30 -0400

    Added a function to add matrices.

--- js102.js
+++ js102.js
@@ -246,3 +246,28 @@ var positiveNumbers = function positiveNumbers(numArray) {
 numbers = [1, -3, 5, -3, 0]
 
 console.log(positiveNumbers(numbers));
+
+// Matrix Addition
+
+var matrix1 = [[2, 4], 
+                [3, 4]];
+var matrix2 = [[5, 2], 
+                [3, 1]];
+
+
+var addMatrix = function addMatrix(mat1, mat2) {
+    var matrixSum = [];
+    var height = mat1.length;
+    var width = mat1[0].length;
+    for (i = 0; i < height; i++) {
+        matrixSum.push([]);
+        for (j = 0; j < width; j++) {
+            var total = (mat1[i][j] + mat2[i][j]);
+            matrixSum[i].push(total);
+        }
+    }
+    return matrixSum
+};
+   
+
+console.log(addMatrix(matrix1, matrix2));
